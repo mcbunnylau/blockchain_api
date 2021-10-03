fc54282ba237095dec77f9e13b015b56cf185002
mcbunnylau <mcbunnylau2013@gmail.com>
2021-10-03 05:05:33 +0100

    feat: created and linked Heroku PostgreSQL database

--- blockchain_api/settings.py
+++ blockchain_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,6 +82,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+# database for AWS RDS Postgres noveltune_database
+
+
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': config("DB_NAME"),
+    'USER': config("DB_USER"),
+    'PASSWORD': config("DB_PASSWORD"),
+    'HOST': config("DB_HOST"),
+    'PORT': config("DB_PORT"),
 }
 
 
